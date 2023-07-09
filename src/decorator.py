071f93abb11e8f7280f6400d23514a24e6330cda
likelion-Jiyeong <julia043130@gmail.com>
2023-07-09 17:48:23 +0900

    [python] 유용한 decorator

--- src/decorator.py
+++ src/decorator.py
@@ -67,10 +67,35 @@ def make_averager():
         """
         count, total은 이 함수에서 선언한 local 변수가 아닌 nonlocal 변수이므로,
         write 수행할 수 없다. nonlocal 선언해주면 write 수행이 가능해진다.
+        클로저는 간단히 말해 함수 안에 내부 함수를 구현하고 그 내부 함수를 반환하는 함수이다.
+        이 때 외부함수는 자신이 가진 값 등을 내부함수에 전달하여 실행하도록 한다.
+
         """
 
-        nonlocal count, total
+        # nonlocal count, total
         count += 1
         total += new_value
         return total/count
-    
+
+
+# functools.lru_cache 데커레이터 : 재귀 알고리즘, 웹에서 정보를 가져와야하는 어플리케이션에서 유용
+
+# 싱글 디스패치 Generic 
+"""functools.singledispatch 모듈로 나왔지만, 이후에 정식으로 라이브러리로 출시되서 pip 로 설치가능하다.
+   자바의 메서드 오버로딩을 파이썬에 적용하기 위해 설계된 것이 아닌, 장황하게 if/elif 블록을 가진 단일 
+   함수 생성을 방지하기 위함이다.
+"""
+from functools import singledispatch
+
+@singledispatch
+def func(x):
+    print("This is default action: ", x)
+
+@func.register(int)
+    # x가 정수인 경우 아래 코드 실행
+def _(x):
+    print("This is for Integer:", x)
+
+@func.register(str)
+def _(x):
+    print("This is for String", x)
